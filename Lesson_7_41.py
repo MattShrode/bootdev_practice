8d503c991a2fba58ab95c40e21c3138d30ecacd7
Matt Shrode <mattshrode@gmail.com>
2025-09-30 23:56:13 -0500

    Work on Lesson 7-41

---
+++ Lesson_7_41.py
@@ -0,0 +1,11 @@
+test_dates = [
+            "07-21-2023",
+            "12-25-2022",
+            "01-01-2023",
+            "01-15-2023",
+            "10-31-2023",
+            "04-10-2023",
+        ]
+
+date_list = list((map(lambda str: str.split("-"), test_dates)))
+print(date_list)
